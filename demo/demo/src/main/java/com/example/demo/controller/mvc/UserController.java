3dbf02d53c6e090522984cc7a2026859c00fe39a
Alejandro Vargas Sánchez <chrisderft@gmail.com>
2026-03-18 19:47:45 -0500

    juan

--- demo/demo/src/main/java/com/example/demo/controller/mvc/UserController.java
+++ demo/demo/src/main/java/com/example/demo/controller/mvc/UserController.java
@@ -1,10 +1,18 @@
 package com.example.demo.controller.mvc;
 
+import java.sql.Timestamp;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.model.Role;
+import com.example.demo.model.User;
+import com.example.demo.service.IRoleService;
 import com.example.demo.service.IUserService;
 
 import lombok.RequiredArgsConstructor;
@@ -15,10 +23,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
     private final IUserService userService;
+    private final IRoleService roleService;
 
     @GetMapping
     public String getAllUsers(Model model) {
         model.addAttribute("users", userService.findAll());
         return "users/list";
+    }
+
+    @GetMapping("/add")
+    public String addForm(Model model) {
+        User user = new User();
+        List<Role> roles = roleService.findAll();
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        return "users/add";
+    }
+
+    @PostMapping
+    public String addUser(@ModelAttribute User user) {
+        user.setCreatedAt(new Timestamp(System.currentTimeMillis()));
+        userService.saveUser(user);
+        return "";
     }
 }
