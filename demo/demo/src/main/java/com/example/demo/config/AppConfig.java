eff782931bf33169fa6213f3b6c0ad2a4cd2b7cb
Alejandro Vargas Sánchez <chrisderft@gmail.com>
2026-04-06 21:17:12 -0500

    app

--- demo/demo/src/main/java/com/example/demo/config/AppConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/AppConfig.java
@@ -5,18 +5,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity(debug = true)
 public class AppConfig {
     @Bean
-    public UserDetailsPasswordService userDetailsService() {
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        UserDetails user = User.withUsername("admin").password("123456").roles("ADMIN").build();
-        userDetailsManager.createUser(user);
-        return userDetailsManager;
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("admin").password("holamundo123").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user);
     }
     
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
