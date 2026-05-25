e2bebf06723e89b4ff08634a9880958f86bda570
Alejandro Vargas Sánchez <chrisderft@gmail.com>
2026-05-25 18:38:08 -0500

    ASZS

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,7 @@
+export default function ProtectedRoute() { 
+    return (<div>
+        <h1>Protected Route</h1>
+        <p>This is a protected route. You must be logged in to view this page.</p>
+    </div>
+    );
+}
